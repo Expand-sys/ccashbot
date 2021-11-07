8e7a2f1901c94f5f9fae76ef7ea989aa3a587c51
Expand-sys <expand@ur-mom.gay>
2021-11-07 19:06:00 +1100

    increase ram

--- commands/moderation/start.js
+++ commands/moderation/start.js
@@ -21,7 +21,7 @@ function spawnMC(channel) {
   };
   const minecraft = spawn(
     "java",
-    ["-Xmx4096M", "-Xms1024M", "-jar", "server.jar", "-nogui"],
+    ["-Xmx24G", "-Xms1024M", "-jar", "server.jar", "-nogui"],
     options
   );
   minecraft.stdout.on("data", (data) => {
